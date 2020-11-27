ded4d0901e74ce9873b6216d76f7a674c61961a8
Maxwellmcl <72890343+Maxwellmcl@users.noreply.github.com>
2020-11-27 11:08:09 -0700

    Update TeacherPage.php

--- TeacherPage.php
+++ TeacherPage.php
@@ -1,3 +1,92 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<button>Logout</button>
+
+<h1>Welcome to the Teacher Homepage</h1>
+
+<h3>Student Data:</h3>
+
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+table, th, td {
+  border: 1px solid black;
+}
+</style>
+</head>
+<body>
+
+
+<table style="width:100%">
+  <tr>
+    <th>Name</th>
+    <th>Car Math Game</th> 
+    <th>Safari Math Game</th>
+  </tr>
+  <tr>
+    <td>Alan</td>
+    <td>null</td>
+    <td>null</td>
+  </tr>
+  <tr>
+    <td>Max</td>
+    <td>null</td>
+    <td>null</td>
+  </tr>
+  <tr>
+    <td>Langsotn</td>
+    <td>null</td>
+    <td>null</td>
+  </tr>
+</table>
+
+</body>
+</html>
+
+<html>
+<body>
+
+<h3>Change Difficulty:</h3>
+//Add image
+
+</body>
+</html>
+
+
+</style>
+</head>
+<body>
+
+
+<table style="width:100%">
+  <tr>
+    <th>Car Math Game</th>
+    <th>Safari Math Game</th> 
+  </tr>
+  <tr>
+    <td><button>Easy</button></td>
+    <td><button>Easy</button></td>
+  </tr>
+  <tr>
+    <td><button>Medium</button></td>
+    <td><button>Medium</button></td>
+     <tr>
+    <td><button>Hard</button></td>
+    <td><button>Hard</button></td>
+</table>
+
+</body>
+</html>
+
+
 <?php
 
 //destroy session method
