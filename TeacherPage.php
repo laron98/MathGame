ffdb98f183af7cf3cb49c5fb1aebcd714823e9f2
Maxwellmcl <72890343+Maxwellmcl@users.noreply.github.com>
2020-12-02 18:55:20 -0700

    Update TeacherPage.php

--- TeacherPage.php
+++ TeacherPage.php
@@ -1,3 +1,15 @@
+function check_CGscore($username){
+	$tempo = getData(USERFILE);
+	print($tempo[$username]['carGameScore']);
+    }
+function check_SGscore($username){
+	$tempo = getData(USERFILE);
+	print($tempo[$username]['ASGameScore']);
+    }
+    
+// need to add function for difficulties
+
+
 <!DOCTYPE html>
 <html>
 <body>
@@ -33,18 +45,18 @@ table, th, td {
   </tr>
   <tr>
     <td>Alan</td>
-    <td>null</td>
-    <td>null</td>
+    <td>check_CGscore(Alan)</td>
+    <td>check_SGscore(Alan)</td>
   </tr>
   <tr>
     <td>Max</td>
-    <td>null</td>
-    <td>null</td>
+    <td>check_CGscore(Max)</td>
+    <td>check_SGscore(Max)</td>
   </tr>
   <tr>
-    <td>Langsotn</td>
-    <td>null</td>
-    <td>null</td>
+    <td>Langston</td>
+    <td>check_CGscore(Langston)</td>
+    <td>check_SGscore(Langston)</td>
   </tr>
 </table>
 
@@ -55,8 +67,6 @@ table, th, td {
 <body>
 
 <h3>Change Difficulty:</h3>
-//Add image
-
 </body>
 </html>
 
@@ -69,18 +79,15 @@ table, th, td {
 <table style="width:100%">
   <tr>
     <th>Car Math Game</th>
-    <th>Safari Math Game</th> 
+    <th><button>Easy</button></th> 
+    <th><button>Hard</button></th>
   </tr>
   <tr>
-    <td><button>Easy</button></td>
-    <td><button>Easy</button></td>
+    <th>Safari Math Game</th>
+    <th><button>Easy</button></th> 
+    <th><button>Hard</button></th> 
   </tr>
-  <tr>
-    <td><button>Medium</button></td>
-    <td><button>Medium</button></td>
-     <tr>
-    <td><button>Hard</button></td>
-    <td><button>Hard</button></td>
+  
 </table>
 
 </body>
@@ -88,65 +95,3 @@ table, th, td {
 
 
 <?php
-
-//destroy session method
-// Logout
-if(isset($_GET['method'])){
-    if($_GET[',ethod'] === 'logout'){
-        destroy_session();
-        }
-    }
-}
-
-
-//print "- Student Scores -"
-//Print Array from Both games (carArray, animalArray)
-//Print key and value, then use key to find second value from other array and print that
-
-//  EX:
-name        Car Game        Animal Game 
-John        87              89
-Emily       93              84
-
-//$cars=array("Volvo","BMW","Toyota");
-//I need to make the diplay put the arrays next to each other.
-//The arrays need a contructor to make blank array so it has values
-//not matter what to print out
-
-function display($studentArray, $carGameArray, $safariGameArray)
-{
-$stuLength = count($studentArray)
-$carLength = count($carGameArray)
-$safariLength = count($safariGameArray)
-
-for($x = 0; $x < $stuLength; $x++)
-    {   
-    echo $studentArray[$x];
-    echo "<br>";
-    }
-    for($x = 0; $x < $carLength; $x++)
-    {   
-    echo $carGameArray[$x];
-    echo "<br>";
-    }
-    for($x = 0; $x < $safariLength; $x++)
-    {   
-    echo $safariGameArray[$x];
-    echo "<br>";
-    }
-}
-
-
-//Below this will be difficulty changes
-Car Game:       -Easy   -Med    -Hard
-Animal Game:    -East   -Med    -Hard
-
-//if user clicks this set variable to 0  1 or  2, so when the game calls for the difficulty it finds a value
-//and uses that to set the range of values. 
-
-function changeDifficulty()
-{
-$carGameDiff = 0;
-$safariGameDiff = 0;
-
-}
