05618ebd7aa0e8e7129d778100904ff54056e720
Maxwellmcl <72890343+Maxwellmcl@users.noreply.github.com>
2020-12-03 20:22:27 -0700

    Update ASGame.php

--- ASGame.php
+++ ASGame.php
@@ -1,7 +1,16 @@
-<?php
-    /*
-    *How to
-    
-    */
-?>
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Safari Math Game</h1>
 
+<div style="background-image: url('SafariBackground.jpg');">
+
+<img src="Animal1.jpg" alt="Animal1" width="100" height="140">
+<img src="Animal2.jpg" alt="Animal2" width="100" height="140">
+
+<p>Which Animal has a ... in the ... place?</p>
+</div>
+
+
+</body>
+</html>
