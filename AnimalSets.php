9a89f479d15e53f48dd3e09a1a5744803bcb311c
Maxwellmcl <72890343+Maxwellmcl@users.noreply.github.com>
2020-12-03 20:07:32 -0700

    Update AnimalSets.php

--- AnimalSets.php
+++ AnimalSets.php
@@ -1,32 +1,38 @@
 <?php
-     //logging user in: prompt for and retrieve username, password, email?
-
-     //select game: 
+   
      echo "Which math skills would you like to practice?<br>";
      //display two buttons with graphics of the games
      echo "Addition and subtraction OR Identifying ones, tens, and hundreds places<br>";
-
-     //safari set
      
+     //safari set
+     function numbers($difficulty){
+          
+          $randomNum = rand(1, $difficulty);
+          
+     }
+
+     function randomNumber(){
+          
+     }
+
+
+     function get_difficulty(){
+          
+     }
+
 
      $difficulty = "medium";
 
      function assignSet($difficulty) {
           
-          $easySet = range(1, 55);
-          $mediumSet = range(55, 99);
-          $hardSet = range(100, 555);
-          $extremeSet = range(555, 999);
+          $easySet = range(1, 99);
+          $hardSet = range(1, 999);
 
           if ($difficulty === "easy") {
                return $easySet;
-          } elseif ($difficulty === "medium") {
-               return $mediumSet;
+          }
           } elseif ($difficulty === "hard") {
                return $hardSet;
-          } elseif ($difficulty === "extreme") {
-               return $extremeSet;
-          } 
      }
      $studentSet = assignSet($difficulty);
      print_r($studentSet);
